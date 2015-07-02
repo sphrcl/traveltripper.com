5e4cb2ec2398decc4fce35cdc8337db1db9f18c2
glitterminded <glitterminded@gmail.com>
2015-07-02 16:22:45 +0800

    homepage markup.. to be continued

--- html/index.php
+++ html/index.php
@@ -8,20 +8,33 @@ include('header.php');
 
 ?>
 
-<section>
+	<div id="banner">
+	
+		<div class="container">
+	
+		<h2>Be Direct.</h2>
+		<p>Innovative hotel digital solutions that increase<br/>direct reservations and maximize revenue.</p>
+		<a class="button1" href="#">LEARN MORE</a>
+		
+		</div>
+		
+	</div>
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
+
+	<section>
+
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
 
 
-</section>
+	</section>
 
 <?php include('footer.php'); ?>
