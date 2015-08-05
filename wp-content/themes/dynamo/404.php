ba2905264e84a9aa47898eb809bdef9f21bec3bc
sphrcl <sphericalcommunications@gmail.com>
2015-08-05 09:33:04 -0400

    adding 404 page

--- wp-content/themes/dynamo/404.php
+++ wp-content/themes/dynamo/404.php
@@ -2,29 +2,17 @@
 
 <div id="title-container">
     <div class="title-block">
-        <h2><?php _e("(404) The page you are looking for does not exist.", "ocmx"); ?></h2>
+       <!-- <h2><?php _e("(404) The page you are looking for does not exist.", "ocmx"); ?></h2> -->
     </div>
 </div>
 
 <div id="content" class="clearfix">
-    <div id="left-column">
-        <ul class="post-list">
-        	<?php $wpquery = new WP_Query(array('post_type' => 'post')); ?>
-			<?php if ($wpquery->have_posts()) :
-                while ($wpquery->have_posts()) :
-                	$wpquery->the_post(); setup_postdata($post);
-					get_template_part("/functions/post-list");
-                endwhile;
-            else :
-                ocmx_no_posts();
-            endif; ?>
-        </ul>
-        <?php motionpic_pagination("clearfix", "pagination clearfix"); ?>
+    <div id="full-width" class="clearfix">                    
+        <div class="post-content clearfix error">
+            <h1>404</h1>
+            <p>Whoops, we can't find what you're looking for.</p>
+            <p><a href="http://www.traveltripper.com/">Back to homepage</a></p>
+        </div>
     </div>
-
-    <?php if(get_option("ocmx_sidebar_layout") != "sidebarnone"): ?>
-        <?php get_sidebar(); ?>
-    <?php endif;?>
 </div>
-
 <?php get_footer(); ?>
