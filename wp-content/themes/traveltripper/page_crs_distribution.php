747976e89940ece2c13c2e4333d1ec9bd0601a82
Jhelline <jhelline.fausto@outsourced.ph>
2015-09-17 01:13:09 +0800

    CRS & Distribution content copy

--- wp-content/themes/traveltripper/page_crs_distribution.php
+++ wp-content/themes/traveltripper/page_crs_distribution.php
@@ -45,51 +45,54 @@ if ($imgsrc) {
 		
 			<div class="col_one_third">
 				<div class="spacing">
-					<h3>Overview of CRS</h3>
-					<h4>Best-in-class booking engine</h4>
+					<h3>Your complete central reservation system</h3>
 					
-					<p>The industry’s most innovative hotel reservations solution. Maecenas ut cursus ligula. ROI pharetra consequat dolor non. semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin. </p>
+					<p>Our CRS is designed to help you make the smartest pricing and distribution decisions for your hotel. Manage rates and inventory all from one dashboard, and connect seamlessly to all major distribution channels. </p>
 				</div>
 			</div>
 			
 			<div class="col_one_third">
 				<ul class="icon44">
+					<li class="icon44-computer">
+						<h5>Innovative booking engine</h5>
+						<p>The industry’s most advanced <u>booking engine</u>, packed with incredible features that drive high conversion rates and increase direct bookings</p>
+					</li>
 					<li class="icon44-dollar">
 						<h5>VERSATILE PRICING</h5>
-						<p>The industry’s most innovative hotel reservations solution maecenas ut.</p>
-					</li>
-					<li class="icon44-property">
-						<h5>MULTI-PROPERTY MANAGEMENT</h5>
-						<p>Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper.</p>
-					</li>
-					<li class="icon44-computer">
-						<h5>CONTENT MANAGEMENT</h5>
-						<p>The industry’s most innovative hotel reservations solution maecenas ut.</p>
+						<p>Manage rates easily and intelligently through our dynamic pricing module</p>
 					</li>
 					<li class="icon44-arrows">
 						<h5>CHANNEL MANAGEMENT</h5>
-						<p>The industry’s most innovative maecenas ut cursus ligula ROI pharetra.</p>
+						<p>Push inventory to all major OTAs, GDS/IDS, Consortia, and Corporate/RFP channels</p>
+					</li>
+					<li class="icon44-metasearch">
+						<h5>METASEARCH CONNECTIVITY</h5>
+						<p>Directly connect to major metasearch sites, including TripAdvisor, Kayak, Trivago, and more</p>
+					</li>
+					<li class="icon44-pci">
+						<h5>PCI compliant </h5>
+						<p>Travel Tripper is fully certified and meets all international PCI data security standards</p>
 					</li>
 				</ul>
 			</div>
 			
 			<div class="col_one_third">
 				<ul class="icon44">
-					<li class="icon44-metasearch">
-						<h5>METASEARCH CONNECTIVITY</h5>
-						<p>The industry’s most innovative hotel reservations solution maecenas ut.</p>
-					</li>
 					<li class="icon44-user">
 						<h5>CALL CENTER</h5>
-						<p>The industry’s most innovative maecenas ut cursus ligula ROI pharetra.</p>
+						<p>Ensure 24/7 multilingual reservations service with high conversions and low call abandonment rates</p>
+					</li>
+					<li class="icon44-property">
+						<h5>MULTI-PROPERTY MANAGEMENT</h5>
+						<p>Manage multiple hotels within your hotel group from one CRS dashboard</p>
+					</li>
+					<li class="icon44-computer">
+						<h5>CONTENT MANAGEMENT</h5>
+						<p>Simplify the management of hotel and room descriptions, images, and policies for all distribution channels</p>
 					</li>
 					<li class="icon44-interface">
 						<h5>PMS/RMS Interfaces</h5>
-						<p>Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper.</p>
-					</li>
-					<li class="icon44-pci">
-						<h5>PCI Compliance</h5>
-						<p>The industry’s most innovative hotel reservations solution maecenas ut.</p>
+						<p>Connect seamlessly to the leading PMS and RMS companies to send availability and reservations</p>
 					</li>
 				</ul>
 			</div>
@@ -98,9 +101,9 @@ if ($imgsrc) {
 			
 			<div class="module2">
 				<div class="spacing2">
-					<h2>34%</h2>
+					<h2>84%</h2>
 					<div class="modulecontent2">
-						<p>We increased Parkside Hotel conversion rates by 34%</p>
+						<p>Find out how Mayfair Hotel & Spa in Miami increased website revenue by 84% after switching to Travel Tripper CRS.</p>
 						<a href="#">LEARN MORE</a>
 					</div>
 				</div>
@@ -122,16 +125,16 @@ if ($imgsrc) {
 	
 					<h3>Powerful pricing tools to help you maximize revenue</h3>
 					
-					<p>Powerful intelligence reporting. Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin.</p>
+					<p>Travel Tripper’s central reservation system allows hotels to manage rates and special offers with an unprecedented degree of granularity. Set specific rates based on geography, booking dates, length of stay, type of device, and more.</p>
 					
 					<ul>
 						<li>
 							<h4>Geo-targeted pricing</h4>
-							<p>The industry’s most innovative hotel reservations solution. Maecenas ut cursus ligula. ROI pharetra.</p>
+							<p>Using our geographic pricing tools, penetrate new markets with special rates and offers without diluting ADR from the rest of the world, and prevent competitor hotels from knowing your global pricing strategy. </p>
 						</li>
 						<li>
 							<h4>Dynamic pricing module</h4>
-							<p>The industry’s most innovative hotel reservations solution. Maecenas ut cursus ligula. ROI pharetra.</p>
+							<p>Easily create multiple rate plans, promotions, and special offers targeted to specific markets, and the booking engine will serve up the most attractive offer to guests who meet the qualifying criteria for the promotion.</p>
 						</li>
 					</ul>
 				
@@ -159,9 +162,11 @@ if ($imgsrc) {
 	
 					<h3>PMS/RMS integrations</h3>
 					
-					<p>Powerful intelligence reporting. Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin. </p>
+					<p>Travel Tripper CRS interfaces with many of the major PMS/RMS companies in the industry, allowing for seamless reservation delivery and rate and inventory updates. </p>
+
+					<p>Don’t see your property management system on the list?  Contact us to discuss integration possibilities. </p>
 					
-					<a class="button1" href="#">LEARN MORE</a>
+					<!--<a class="button1" href="#">LEARN MORE</a>-->
 				
 				</div>
 				
@@ -170,15 +175,19 @@ if ($imgsrc) {
 			<div class="col_two_third">
 				
 				<ul class="checklist">
-					<li><span class="checkbutton">Autoclerk</span></li>
-					<li><span class="checkbutton">Maestro</span></li>
-					<li><span class="checkbutton">Roommaster</span></li>
-					<li><span class="checkbutton">Champagne</span></li>
 					<li><span class="checkbutton">Opera</span></li>
-					<li><span class="checkbutton">Springer Miller</span></li>
-					<li><span class="checkbutton">Epitomie</span></li>
-					<li><span class="checkbutton">Protel</span></li>
 					<li><span class="checkbutton">Visual One</span></li>
+					<li><span class="checkbutton">RezNet</span></li>
+					<li><span class="checkbutton">Roommaster</span></li>
+					<li><span class="checkbutton">Maestro</span></li>
+					<li><span class="checkbutton">Epitomie</span></li>
+					<li><span class="checkbutton">Lodgical</span></li>
+					<li><span class="checkbutton">Champagne</span></li>
+					<li><span class="checkbutton">Autoclerk</span></li>
+					<li><span class="checkbutton">WebRezPro</span></li>
+					<li><span class="checkbutton">Springer Miller</span></li>
+					<li><span class="checkbutton">Protel</span></li>
+					<li><span class="checkbutton">iDeaS – RMS</span></li>
 				</ul>
 				
 			</div>
@@ -191,9 +200,8 @@ if ($imgsrc) {
 	
 		<div class="container">
 	
-			<h3>Dedicated account service</h3>
-			<h4>Talk to a person, not a ticket</h4>
-			<p>Powerful intelligence reporting. Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin. </p>
+			<h3>Dedicated and personal account service<br />from our expert team</h3>
+			<p>Travel Tripper’s CRS is supported by dedicated team of revenue optimization and marketing experts that will guide your hotel to success. Our integrated client services and customer support teams allow for a single point of contact so you can quickly get the answers and help that you need. </p>
 			
 		</div>
 		
@@ -203,7 +211,8 @@ if ($imgsrc) {
 		<div class="container">
 		
 			<div class="col_one_fourth">
-				<h3>Book more. Be direct.</h3>
+				<h3>It’s time to be more direct.</h3>
+				<p class="cta-text">Find out how Travel Tripper can help your hotel increase direct bookings and maximize revenue.</p>
 			</div>
 			
 			<form name="" action="#" method="get">
