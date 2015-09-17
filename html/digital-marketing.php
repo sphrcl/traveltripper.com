6e6d59ecea086b8e6940ed85fce2767951a8fcff
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-09-17 22:19:48 +0800

    digital marketing content for markup

--- html/digital-marketing.php
+++ html/digital-marketing.php
@@ -34,16 +34,19 @@ include('header.php');
 			<div class="col_one_fourth">
 				<div class="spacing">
 					<h3>Target audiences with precision</h3>
-					<p>The industry’s most innovative hotel reservations solution. Maecenas ut cursus ligula. ROI pharetra consequat dolor non. semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin.</p>
 					
 					<ul class="icon44">
 						<li class="icon44-www">
-							<h4>PCC MANAGEMENT</h4>
-							<p>The industry’s most innovative maecenas ut cursus ligula ROI pharetra maecenas ut</p>
+							<h4>PPC Search Marketin</h4>
+							<p>Travel Tripper’s pay-per-click strategic management can help your hotel increase interested traffic through geo-targeted, cost-effective campaigns. Our expertise includes market analysis, keyword targeting, bidding, ad and landing page development, and budgeting. </p>
 						</li>
-						<li class="icon44-search">
-							<h4>SEARCH MARKETING</h4>
-							<p>The industry’s most innovative maecenas ut cursus ligula ROI pharetra maecenas ut</p>
+						<li class="icon44-computer">
+							<h4>Site Retargeting</h4>
+							<p>Our hotel remarketing solutions include email, site, and search retargeting services. Our digital marketing team will guide and manage hotel retargeting campaigns, including ad placement, landing page development, and budgeting. </p>
+						</li>
+						<li class="icon44-metasearch">
+							<h4>Metasearch Placement</h4>
+							<p>Drive traffic from the major travel metasearch sites, such as TripAdvisor, Google, Kayak, Trivago, WeGo and more. Travel Tripper can help you find ways to target searches and place links so you can get the best value for your ad spend. </p>
 						</li>
 					</ul>
 					
@@ -69,9 +72,9 @@ include('header.php');
 			
 			<div class="module2">
 				<div class="spacing2">
-					<h2>45%</h2>
+					<h2>100%</h2>
 					<div class="modulecontent2">
-						<p>Find out how Mayfair Hotel & Spa in Miami more than doubled the ROI of their PPC marketing.</p>
+						<p>Find out how Mayfair Hotel & Spa in Miami more than doubled the ROI of their search marketing after switching to Travel Tripper.</p>
 						<a href="#">LEARN MORE</a>
 					</div>
 				</div>
@@ -87,9 +90,8 @@ include('header.php');
 		
 			<div class="col_one_fourth">
 				<div class="spacing">
-					<h3>Reporting and analytics</h3>
-					<h4>Detailed monitoring of your website and channel performance</h4>
-					<p>Powerful intelligence reporting. Maecenas ut cursus ligula. ROI pharetra consequat dolor non semper. Nam in ligula dictum, ultrices ante vitae, sollicitudin vitae, sollicitudin. </p>
+					<h3>Maximum visibility for minimum cost</h3>
+					<p>Successful digital marketing is not just about generating clicks and bookings—we closely monitor and track marketing spend and ROI to ensure you’re getting meaningful results and making the most of your advertising dollars. </p>
 					
 				</div>
 			</div>
@@ -130,8 +132,8 @@ include('header.php');
 	
 		<div class="container">
 	
-			<h3>Personalized service from one single account team</h3>
-			<p>Unified marketing and distribution strategy. Dedicated team helps you with implementation, training, strategy, ongoing support</p>
+			<h3>Your personal revenue optimization team</h3>
+			<p>With Travel Tripper, you’ll get dedicated, hands-on support from hotel marketing and distribution experts that will guide your hotel to success. Our integrated client services and customer support teams allow for a single point of contact so you can quickly get the help and answers you need. </p>
 			
 		</div>
 		
@@ -141,7 +143,8 @@ include('header.php');
 		<div class="container">
 		
 			<div class="col_one_fourth">
-				<h3>Book more. Be direct.</h3>
+				<h3>It’s time to be more direct.</h3>
+				<p class="cta-text">Find out how Travel Tripper can help your hotel increase direct bookings and maximize revenue.</p>
 			</div>
 			
 			<form name="" action="#" method="get">
