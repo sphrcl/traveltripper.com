7f364c04f9496a739045c99ed34263ec8d39c83d
jeromemisfit <jerome@misfit-inc.com>
2015-09-23 00:39:27 +0800

    solutions changes v1

--- html/solutions.php
+++ html/solutions.php
@@ -32,30 +32,27 @@ include('header.php');
 	
 	<div id="solutionsmain">
 	
+		<div class="solutionstabs1">
 		<div class="container">
-		
-			<div class="pagesection">
-		
-				<h3>The “suite” solution from search to stay</h3>
-		
-			</div>
 			
-			<div class="pagesection">
+			<h3>The “suite” solution from search to stay</h3>
+			
+			<div class="pagesection one">
 		
-				<div class="col_one_third">
+				<div class="col_one_third one">
 					<div class="spacing">
 						<img src="images/icon-blue-crs.png" alt="crs" />
-						<h4>CRS & Distribution</h4>
+						<h4>CRS &amp; Distribution</h4>
 						<p>
 							Travel Tripper CRS is your most powerful tool in managing room rates, inventory, and reservations. 
 							Our platform provides comprehensive hotel distribution to all major channels, seamless connectivity 
 							to the major PMS/RMS services, and robust tools for pricing and revenue management. 
 						</p>
-						<p><a class="button1" href="crs.php">LEARN MORE</a></p>
+						<a class="button1" href="#">LEARN MORE</a>
 					</div>
 				</div>
 				
-				<div class="col_one_third">
+				<div class="col_one_third two">
 					<div class="spacing">
 						<img src="images/icon-blue-booking-engine.png" alt="booking engine" />
 						<h4>Booking Engine</h4>
@@ -64,21 +61,21 @@ include('header.php');
 							With its sleek, user-friendly booking flow and unique features such as Automated Best Rate Guarantee, 
 							this booking engine is designed to optimize conversions through your most profitable channel—your website.
 						</p>
-						<p><a class="button1" href="#">LEARN MORE</a></p>
+						<a class="button1" href="#">LEARN MORE</a>
 					</div>
 				</div>
 				
-				<div class="col_one_third">
+				<div class="col_one_third three">
 					<div class="spacing">
 						<img src="images/icon-blue-web-mobile.png" alt="web mobile" />
-						<h4>Web & Mobile</h4>
+						<h4>Web &amp; Mobile</h4>
 						<p>
 							Introducing Travel Tripper Web, a cloud web and booking platform designed specifically for 
 							hotels to entice travelers and drive direct bookings. Create stunning responsive websites 
 							perfect for web and mobile, and tap into intelligent features such as dynamic rate integration
 							 and interactive maps and calendars.
 						</p>
-						<p><a class="button1" href="#">LEARN MORE</a></p>
+						<a class="button1" href="#">LEARN MORE</a>
 					</div>
 				</div>
 		
@@ -86,9 +83,9 @@ include('header.php');
 			
 			<div class="line1"></div>
 			
-			<div class="pagesection">
+			<div class="pagesection two">
 		
-				<div class="col_one_third">
+				<div class="col_one_third one">
 					<div class="spacing">
 						<img src="images/icon-blue-digital-marketing.png" alt="digital marketing" />
 						<h4>Digital Marketing</h4>
@@ -97,11 +94,11 @@ include('header.php');
 							From managing search marketing to developing custom special offers, our dedicated marketing 
 							team ensures that your hotel gets maximum visibility for minimum cost.
 						</p>
-						<p><a class="button1" href="#">LEARN MORE</a></p>
+						<a class="button1" href="#">LEARN MORE</a>
 					</div>
 				</div>
 				
-				<div class="col_one_third">
+				<div class="col_one_third two">
 					<div class="spacing">
 						<img src="images/icon-blue-revenue-management.png" alt="revenue management" />
 						<h4>Revenue Management</h4>
@@ -111,14 +108,14 @@ include('header.php');
 							Track past performance and future projections, anticipate market demand with flight data, 
 							monitor comp set and market data, and generate detailed reporting all from one dashboard.
 						</p>
-						<p><a class="button1" href="#">LEARN MORE</a></p>
+						<a class="button1" href="#">LEARN MORE</a>
 					</div>
 				</div>
 				
-				<div class="col_one_third">
+				<div class="col_one_third three">
 					<div class="spacing">
 						
-						<h4>Selected Clients & Testimonials</h4>
+						<h4>Selected Clients <br>&amp; Testimonials</h4>
 					
 						<div class="flexslider testiflex">
 							<ul class="slides">
@@ -149,14 +146,12 @@ include('header.php');
 			</div>
 	
 		</div>
+		</div>
 		
-		<div class="container2">
-		
-			<div class="pagesection">
-		
-				<h3>Why choose Travel Tripper?</h3>
-				
-			</div>
+		<div class="solutionstabs2">
+		<div class="container">
+			
+			<h3>Why choose Travel Tripper?</h3>
 			
 			<div class="pagesection">
 				
@@ -193,6 +188,7 @@ include('header.php');
 			
 			</div>
 
+		</div>
 		</div>
 
 	</div>
