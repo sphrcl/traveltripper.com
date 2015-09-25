43522eab6491afcd82d7abe425894c9a749cc122
jeromemisfit <jerome@misfit-inc.com>
2015-09-26 01:05:54 +0800

    booking engine change by client
    - check the evernote docs

--- new-html/booking-engine.php
+++ new-html/booking-engine.php
@@ -34,7 +34,7 @@ include('header.php');
 	<div id="breadcrumbs">
 		<div class="container">
 		<div class="subcontainer">
-			<a href="index.php">Home</a> <i class="fa fa-angle-right"></i> <a href="booking-engine.php">Travel Tripper Booking Engine</a>
+			<a href="index.php">Travel Tripper</a> <i class="fa fa-angle-right"></i> <a href="booking-engine.php">Booking Engine</a>
 		</div>
 		</div>
 	</div>
