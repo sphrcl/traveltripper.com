be35e436f6b1481646b92f076d850cab8dda2c4e
sphrcl <sphericalcommunications@gmail.com>
2015-09-29 16:41:40 -0400

    New page - careers detail and edit to style

---
+++ new-html/finance-admin-associate.php
@@ -0,0 +1,178 @@
+<?php 
+	
+	$description = '';
+	$pagetitle = 'Finance & Admin. Associate';
+	$image = '';
+	$template_class = 'template-base';
+
+include('header.php'); 
+
+?>
+	
+	<div id="pagetitle">
+	
+		<div class="container">
+		<div class="subcontainer">
+	
+			<h1><?php echo $pagetitle; ?></h1>
+		
+		</div>
+		</div>
+		
+	</div>
+
+	<div id="breadcrumbs">
+		<div class="container">
+		<div class="subcontainer">
+			<a href="index.php">Home</a> > <a href="#">Page Template</a>
+		</div>
+		</div>
+	</div>
+	
+	<div id="mainpage">
+	
+		<div class="container">
+		<div class="subcontainer">
+	
+			<div class="pagecontent">
+				
+				<div class="spacing">
+				
+				<div class="entry">
+					
+					<h2>Job Opening: Finance & Admin. Associate</h2>
+					
+					<p>As Finance and Admin. Associate, your responsibilities span multiple areas including finance, HR, administration, operations and sales. This role will provide you with a great opportunity to learn quickly and build your business skills. You’ll be responsible for maintaining customer records, sending out bills, receiving and collecting customer payments, managing our office, hiring and managing company vendors, providing executive support, and serving as our human resource administrator for all our employees.</p>
+					
+					<p>You will need to bring a strong sense of responsibility and be able to work resourcefully and efficiently with little or no supervision in a fast paced environment. You will have expert written and verbal communication skills, strong organizational skills and be able to maintain a high degree of confidentiality.</p>
+
+					<h2>About Travel Tripper</h2>
+
+					<p>Travel Tripper is fast growing and dynamic software company based in Manhattan and serving the hotel industry. Our team brings together expertise in the marketing, technology, and hospitality sectors. We work together collaboratively to create, refine, and then shape new ideas into great products for our customers.</p>
+
+					<p>Working at Travel Tripper is an opportunity to join an exciting company with a great team culture and a successful track record. We are highly selective in our hiring process seeking individuals with demonstrated excellence in their backgrounds. We look for smart people who are passionate about technology and the web, who care deeply about their careers, and who want to work hard and have fun.</p>
+
+					<p>We offer a collaborative and interdependent team environment where you can apply your experience in building world class software across all tiers of our system. We offer full benefits including a Options Plan, Medical, Dental, Vision, 401(k), three weeks vacation, and more. </p>
+
+					<h2>Responsibilities</h2>
+
+					<ul>
+						<li>Invoice using Quickbooks and receive / collect payments from customers</li>
+						<li>Generate regular reports for customers</li>
+						<li>Manage company calendar, set appointments, organizing travel and conferences, and manage expense reports</li>
+						<li>Assist with the preparation of external facing presentation materials and documents</li>
+						<li>Assist with sales prospecting by generating and qualifying leads, monitoring industry publications and events, and constant updates to our customer relationship management database</li>
+						<li>Organize and promote participation to social and team building events</li>
+						<li>Manage incoming and outgoing communication, including external mail and telephone calls to ensure the smooth and timely flow of information in and out of the office</li>
+						<li>Manage office including ordering of supplies and generally maintaining a smoothly functioning fun-place-to-work office</li>
+					</ul>
+					
+					<h2>Requirements</h2>
+
+					<ul>
+						<li>Bachelor’s Degree</li>
+						<li>2-3 years of experience professional experience</li>
+						<li>Familiar with Quickbooks</li>
+						<li>Proficient in MS Office and Google Docs with knowledge of or ability learn quickly</li>
+						<li>Detail oriented and highly organized</li>
+						<li>Excellent verbal and written communication skills, including the ability to draft correspondence with minimal direction</li>
+						<li>Highest level of integrity and ability to handle highly confidential information</li>
+						<li>Excellent verbal and written communication skills</li>
+						<li>Send resumes to <a href="mailto:careers@traveltripper.com">careers@traveltripper.com</a></li>						
+					</ul>
+
+				</div>
+				
+				</div>
+				
+			</div>
+			
+			<div class="sidebar">
+			
+				<div class="widgets search">
+				
+					<form name="" action="#" method="get">
+
+						<input type="text" name="s" value="" placeholder="SEARCH">
+				
+					</form>
+				
+				</div>
+				
+				<div class="widgets social">
+				
+					<ul>
+						<li><a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="http://www.facebook.com/"><i class="fa fa-facebook-square"></i></a></li>
+						<li><a href="http://www.twitter.com/"><i class="fa fa-twitter"></i></a></li>
+					</ul>
+				
+				</div>
+				
+				<div class="widgets newsletter">
+				
+					<h3 class="widgettitle">Join Our Newsletter</h3>
+				
+					<form name="" action="#" method="get">
+						<input type="text" name="email" value="" placeholder="E-MAIL ADDRESS">
+						<input type="submit" name="submit" value="SIGN UP">
+					</form>
+				
+				</div>
+				
+				<div class="widgets featured-posts">
+				
+					<h3 class="widgettitle">FEATURED POSTS</h3>
+				
+					<ul>
+						<li><a href="#">What hotels can learn from Airbnb’s design and user experience</a></li>
+						<li><a href="#">Capture more direct reservations with TripConnect™ Instant Booking</a></li>
+						<li><a href="#">Find the best hotel booking engine: design makes a difference</a></li>
+					</ul>
+
+				</div>
+				
+				<div class="widgets recent-posts">
+				
+					<h3 class="widgettitle">RECENT POSTS</h3>
+				
+					<ul>
+						<li><a href="#">What hotels can learn from Airbnb’s design and user experience</a></li>
+						<li><a href="#">Capture more direct reservations with TripConnect™ Instant Booking</a></li>
+						<li><a href="#">Find the best hotel booking engine: design makes a difference</a></li>
+					</ul>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">News</a></h3>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">Trends</a></h3>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">Whats on our minds</a></h3>
+
+				</div>
+		
+			</div>
+			
+			<div class="clear"></div>
+			
+		</div>
+		</div>
+		
+	</div>
+	
+	<?php include('cta-newsletter.php'); ?>
+	
+	<?php include('bottom-nav.php'); ?>
+
+<?php include('footer.php'); ?>
---
+++ new-html/revenue-analyst.php
@@ -0,0 +1,182 @@
+<?php 
+	
+	$description = '';
+	$pagetitle = 'Revenue Analyst';
+	$image = '';
+	$template_class = 'template-base';
+
+include('header.php'); 
+
+?>
+	
+	<div id="pagetitle">
+	
+		<div class="container">
+		<div class="subcontainer">
+	
+			<h1><?php echo $pagetitle; ?></h1>
+		
+		</div>
+		</div>
+		
+	</div>
+
+	<div id="breadcrumbs">
+		<div class="container">
+		<div class="subcontainer">
+			<a href="index.php">Home</a> > <a href="#">Page Template</a>
+		</div>
+		</div>
+	</div>
+	
+	<div id="mainpage">
+	
+		<div class="container">
+		<div class="subcontainer">
+	
+			<div class="pagecontent">
+				
+				<div class="spacing">
+				
+				<div class="entry">
+
+					<h2>Job Opening: Revenue Analyst</h2>
+					
+					<p>The Revenue Analyst position is primarily responsible for generating online reservations for Travel Tripper’s customers in order to meet and exceed revenue targets and establish a market presence. The job requires a combination of revenue management and tactical online marketing. He/She must also assist Client services team to serve as a point of contact for all the client’s needs from relationship building, customer-service and championing the customer’s product enhancement requirements.</p>
+
+					<h2>About Travel Tripper</h2>
+					
+					<p>Travel Tripper is fast growing and dynamic software company based in Manhattan and serving the hotel industry. Our team brings together expertise in the marketing, technology, and hospitality sectors. We work together collaboratively to create, refine, and then shape new ideas into great products for our customers.</p>
+
+					<p>Working at Travel Tripper is an opportunity to join an exciting company with a great team culture and a successful track record. We are highly selective in our hiring process seeking individuals with demonstrated excellence in their backgrounds. We look for smart people who are passionate about technology and the web, who care deeply about their careers, and who want to work hard and have fun.</p>
+
+					<p>We offer a collaborative and interdependent team environment where you can apply your experience in building world class software across all tiers of our system. We offer full benefits including a Options Plan, Medical, Dental, Vision, 401(k), three weeks vacation, and more.</p>
+
+					<h2>Responsibilities</h2>
+
+					<ul>
+						<li>Understand and internalize Travel Tripper’s products and services</li>
+						<li>Support client services managers with account management of clients</li>
+						<li>Ensure proper implementation of products for new customer hotels</li>
+						<li>Provide support for client support requests across all products</li>
+						<li>Diagnose and duplicate customer reported issuesand manage the resolution of the problem – including ability to communicate with technical team to troubleshoot</li>
+						<li>Develop and maintain relationships with customers</li>
+						<li>Educate customers on Travel Tripper products and new feature releases</li>
+						<li>Assist in generation and distribution of weekly and monthly reports to clients</li>
+						<li>Analyze performance metrics in reports distributed to clients</li>
+						<li>Communicate and follow up with internal teams for enhancement requests, escalated support requests, and marketing inquiries</li>
+						<li>Maintain high level of professionalism when interacting with clients</li>
+						<li>Assist with ad hoc projects as needed to provide client satisfaction and company growth</li>											
+					</ul>
+
+					<h2>Requirements</h2>
+
+					<ul>
+						<li>A Bachelor’s Degree or managerial/supervisory experience in the Hospitality or Travel industry</li>
+						<li>Strong and proven track record in hospitality revenue management or reservations, or other industry with fixed, perishable inventory</li>
+						<li>Demonstrate ability to work based on targets/goals</li>
+						<li>Excellent verbal and written communications skills</li>
+						<li>High interest in working for a fast paced technology firm</li>
+						<li>Very comfortable with large volumes of data and spreadsheets and an ability to perform “back of the envelope” style calculations on the fly</li>
+						<li>2 years of travel industry experience (hospitality experience preferred)</li>
+						<li>Proficiency in Microsoft Office with particularly VERY strong knowledge of Excel required</li>
+						<li>Ability to multi task and prioritize effectively while possessing excellent time management and organizational skills</li>
+						<li>Detail oriented and highly organized</li>
+						<li>Send resumes to <a href="mailto:careers@traveltripper.com">careers@traveltripper.com</a></li>																		
+					</ul>
+					
+				</div>
+				
+				</div>
+				
+			</div>
+			
+			<div class="sidebar">
+			
+				<div class="widgets search">
+				
+					<form name="" action="#" method="get">
+
+						<input type="text" name="s" value="" placeholder="SEARCH">
+				
+					</form>
+				
+				</div>
+				
+				<div class="widgets social">
+				
+					<ul>
+						<li><a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="http://www.facebook.com/"><i class="fa fa-facebook-square"></i></a></li>
+						<li><a href="http://www.twitter.com/"><i class="fa fa-twitter"></i></a></li>
+					</ul>
+				
+				</div>
+				
+				<div class="widgets newsletter">
+				
+					<h3 class="widgettitle">Join Our Newsletter</h3>
+				
+					<form name="" action="#" method="get">
+						<input type="text" name="email" value="" placeholder="E-MAIL ADDRESS">
+						<input type="submit" name="submit" value="SIGN UP">
+					</form>
+				
+				</div>
+				
+				<div class="widgets featured-posts">
+				
+					<h3 class="widgettitle">FEATURED POSTS</h3>
+				
+					<ul>
+						<li><a href="#">What hotels can learn from Airbnb’s design and user experience</a></li>
+						<li><a href="#">Capture more direct reservations with TripConnect™ Instant Booking</a></li>
+						<li><a href="#">Find the best hotel booking engine: design makes a difference</a></li>
+					</ul>
+
+				</div>
+				
+				<div class="widgets recent-posts">
+				
+					<h3 class="widgettitle">RECENT POSTS</h3>
+				
+					<ul>
+						<li><a href="#">What hotels can learn from Airbnb’s design and user experience</a></li>
+						<li><a href="#">Capture more direct reservations with TripConnect™ Instant Booking</a></li>
+						<li><a href="#">Find the best hotel booking engine: design makes a difference</a></li>
+					</ul>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">News</a></h3>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">Trends</a></h3>
+
+				</div>
+				
+				<div class="widgets textwidget">
+				
+					<h3><a href="#">Whats on our minds</a></h3>
+
+				</div>
+		
+			</div>
+			
+			<div class="clear"></div>
+			
+		</div>
+		</div>
+		
+	</div>
+	
+	<?php include('cta-newsletter.php'); ?>
+	
+	<?php include('bottom-nav.php'); ?>
+
+<?php include('footer.php'); ?>
