2cf409051e1835567bd1d4f15d7bc2de7f4bd2e9
sphrcl <sphericalcommunications@gmail.com>
2015-09-29 13:54:09 -0400

    Resources - internal linking

--- new-html/resources.php
+++ new-html/resources.php
@@ -27,36 +27,36 @@ include('header.php');
 		
 			<div class="resourcebox one">
 				<div class="spacing">
-					<a class="resourceimg" href="#" style="background-image: url('images/resources1.jpg');"></a>
+					<a class="resourceimg" target="_blank" href="http://www.traveltripper.com/optimize-your-hotel-website-conversions/spot-the-conversion-killers-eguide/" style="background-image: url('images/resources1.jpg');"></a>
 					<div class="category">ARTICLE</div>
-					<h3><a href="#">Spot The Conversion Killers</a></h3>
+					<h3><a target="_blank" href="http://www.traveltripper.com/optimize-your-hotel-website-conversions/spot-the-conversion-killers-eguide/">Spot The Conversion Killers</a></h3>
 				</div>
 			</div>
 			
 			<div class="resourcebox two">
 				<div class="spacing">
-					<a class="resourceimg" href="#" style="background-image: url('images/resources2.jpg');"></a>
+					<a class="resourceimg" target="_blank" href="http://www.tnooz.com/article/hotels-OTAs-battle-tech-solution/" style="background-image: url('images/resources2.jpg');"></a>
 					<div class="category">ARTICLE</div>
-					<h3><a href="#">Tnooz: Hotels, OTAs battle tech solution</a></h3>
+					<h3><a target="_blank" href="http://www.tnooz.com/article/hotels-OTAs-battle-tech-solution/">Tnooz: Hotels, OTAs battle tech solution</a></h3>
 				</div>
 			</div>
 			
 			<div class="resourcebox three">
 				<div class="spacing">
-					<a class="resourceimg" href="#" style="background-image: url('images/resources3.jpg');"></a>
+					<a class="resourceimg" target="_blank" href="http://skift.com/2015/03/25/will-rethinking-metasearch-increase-direct-bookings-for-hotels/" style="background-image: url('images/resources3.jpg');"></a>
 					<div class="category">ARTICLE</div>
-					<h3><a href="#">Skift: Will Rethinking Metasearch Increase Direct Bookings for Hotels?</a></h3>
+					<h3><a target="_blank" href="http://skift.com/2015/03/25/will-rethinking-metasearch-increase-direct-bookings-for-hotels/">Skift: Will Rethinking Metasearch Increase Direct Bookings for Hotels?</a></h3>
 				</div>
 			</div>
 			
 			<div class="resourcebox four">
 				<div class="spacing">
-					<a class="resourceimg" href="#" style="background-image: url('images/resources4.jpg');"></a>
+					<a class="resourceimg" target="_blank" href="http://skift.com/2015/04/23/paradox-of-choice-do-too-many-options-kill-conversion-rate/" style="background-image: url('images/resources4.jpg');"></a>
 					<div class="category">ARTICLE</div>
-					<h3><a href="#">Skift: Paradox of Choice</a></h3>
+					<h3><a target="_blank" href="http://skift.com/2015/04/23/paradox-of-choice-do-too-many-options-kill-conversion-rate/">Skift: Paradox of Choice</a></h3>
 				</div>
 			</div>
-			
+			<!--
 			<div class="resourcebox one">
 				<div class="spacing">
 					<a class="resourceimg" href="#" style="background-image: url('images/resources1.jpg');"></a>
@@ -88,7 +88,7 @@ include('header.php');
 					<h3><a href="#">Skift: Paradox of Choice</a></h3>
 				</div>
 			</div>
-		
+			-->
 		</div>
 		</div>
 		
