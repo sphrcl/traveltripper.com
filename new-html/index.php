1ae1547cd29abdfa6b1fb24dc78028f2a8c91fa1
sphrcl <sphericalcommunications@gmail.com>
2015-09-29 15:21:56 -0400

    Homepage - internal linking

--- new-html/index.php
+++ new-html/index.php
@@ -138,7 +138,7 @@ include('header.php');
 						<div class="bkcnt-block">
 							<h2>Web &amp; Mobile</h2>
 							<p>A powerful booking platform becomes even stronger with a beautiful website. Travel Tripper Web, our new cloud web and booking platform, allows you or your design agency to create stunning responsive websites that are perfectly optimized across desktop and mobile devices. Dynamic rate integration with Travel Tripper CRS and booking engine makes managing e-commerce a breeze.</p>
-							<a class="button1" href="#">LEARN MORE</a>
+							<a class="button1" href="web-mobile.php">LEARN MORE</a>
 						</div>
 					</div>
 				</div>
