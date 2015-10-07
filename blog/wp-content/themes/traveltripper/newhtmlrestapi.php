634472b7c7d04e5c771ad4982ce23041f3dce586
jeromemisfit <jerome@misfit-inc.com>
2015-10-07 12:28:35 +0800

    ##### api connect* v1
    - * needs to be setup when site is live

--- blog/wp-content/themes/traveltripper/newhtmlrestapi.php
+++ blog/wp-content/themes/traveltripper/newhtmlrestapi.php
@@ -1,14 +1,14 @@
 
-<?php
+<?php 
 
+/*  Template Name: API Connect  */
 // http://blog.ijasoneverett.com/2013/02/rest-api-a-simple-php-tutorial/
 
-
-
-
-
-
-
-
+if (isset($_GET["actionpass"]) && ($_GET["actionpass"] == 'traveltripper')) {
+	get_sidebar();
+} else {
+	header('Location:' . get_bloginfo('url'));
+	die();
+}
 
 ?>
