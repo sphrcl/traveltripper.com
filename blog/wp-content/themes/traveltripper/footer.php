5ed72371a7d7bee47f0dd64e8f044e9650d54c5b
jeromemisfit <jerome@misfit-inc.com>
2015-10-07 17:32:29 +0800

    footer changes

--- blog/wp-content/themes/traveltripper/footer.php
+++ blog/wp-content/themes/traveltripper/footer.php
@@ -10,50 +10,51 @@
 		<div id="footer">
 		
 			<div class="container">
+			<div class="subcontainer">
 			
-				<div class="col_one_fourth">
+				<div class="col_one_fourth one">
 					<div class="footernav">
-						<a href="index.php"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="travel tripper" /></a>
+						<a href="index.php"><img src="images/traveltripperlogo.png" alt="travel tripper" /></a>
 						<br/><br/><br/><br/>
 						<p>&copy; 2015 Travel Tripper Inc.</p>
 					</div>
 				</div>
 				
-				<div class="col_one_fourth">
+				<div class="col_one_fourth two">
 					<div class="footernav">
 						<ul>
-							<li><a href="company.php">Company</a></li>
-							<li><a href="product-services.php">Product & Services</a></li>
+							<li><a href="aboutus.php">Company</a></li>
+							<li><a href="product-and-services.php">Product &amp; Services</a></li>
 							<li><a href="careers.php">Careers</a></li>
-							<li><a href="terms.php">Terms of Service</a></li>
-							<li><a href="privacy-policy.php">Privacy Policy</a></li>
 						</ul>
 					</div>
 				</div>
 				
-				<div class="col_one_fourth">
+				<div class="col_one_fourth three">
 					<div class="footeradd">
 						<i class="fa fa-map-marker"></i>
 						<p>370 Lexington Avenue,<br/>
-						Suite 1601, New York, NY 10017<br/><br/>
-						
+						Suite 1601, New York, NY 10017<br/>
 						+ 1.212.683.6161</p>
 					</div>
 				</div>
 				
-				<div class="col_one_fourth">
+				<div class="col_one_fourth four">
 					<div class="footersocial">
 						<ul>
-							<li><a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
-							<li><a href="http://www.facebook.com/"><i class="fa fa-facebook-square"></i></a></li>
-							<li><a href="http://www.twitter.com/"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="https://www.linkedin.com/company/travel-tripper" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="https://www.facebook.com/traveltripper" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
+							<li><a href="https://twitter.com/Travel_Tripper" target="_blank"><i class="fa fa-twitter"></i></a></li>
 						</ul>
 						
-						<p><a href="#">Say Hello &rarr;</a></p>
+						<p><a href="mailto:hello@traveltripper.com">Say Hello &rarr;</a></p>
 						
 					</div>
 				</div>
 				
+				<div class="clear"></div>
+				
+			</div>
 			</div>
 		</div>
 
