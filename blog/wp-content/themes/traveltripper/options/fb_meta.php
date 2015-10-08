9db134cb2daa305c04af2c7dff21a742ccb50f68
jeromemisfit <jerome@misfit-inc.com>
2015-10-08 11:28:48 +0800

    fb meta changes

--- blog/wp-content/themes/traveltripper/options/fb_meta.php
+++ blog/wp-content/themes/traveltripper/options/fb_meta.php
@@ -3,18 +3,20 @@
 // https://www.elegantthemes.com/blog/tips-tricks/how-to-add-open-graph-tags-to-wordpress
 
 function fb_opengraph() {
-    global $post;
- 
-    if(is_single()) {
-        if(has_post_thumbnail($post->ID)) {
-            $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'large');
-        } else {
-            // $img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
-        }
-		
-		$excerpt = get_the_excerpt();
-		
-        ?>
+
+global $post;
+
+if(is_single()) {
+	
+	if(has_post_thumbnail($post->ID)) {
+		$img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'large');
+	} else {
+		// $img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
+	}
+	
+	$excerpt = get_the_excerpt();
+	
+?>
  
     <meta property="og:title" content="<?php echo the_title(); ?>"/>
     <meta property="og:description" content="<?php echo $excerpt; ?>"/>
@@ -23,11 +25,34 @@ function fb_opengraph() {
     <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
     <meta property="og:image" content="<?php echo $img_src[0]; ?>"/>
  
-<?php
-    } else {
-        return;
-    }
-}
-add_action('wp_head', 'fb_opengraph', 5);
+<?php } else {
+	
+	$img_src = get_template_directory_uri() . '/images/traveltripperlogo.png';
+	
+	if (get_the_excerpt() != '') {
+		$excerpt = get_the_excerpt();
+	} else {
+		$excerpt = 'Discover Travel Tripper’s powerful hotel platforms designed to increase direct reservations and maximize revenue';
+	}
+	
+	if (is_home()) {
+		$fb_title = 'Travel Tripper';
+	} else {
+		$fb_title = get_the_title();
+	}
+	
+?>
 
+    <meta property="og:title" content="<?php echo $fb_title; ?>"/>
+    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+	
+<?php } ?>
+
+
+<?php } // END FUNCTION
+add_action('wp_head', 'fb_opengraph', 5);
 ?>
