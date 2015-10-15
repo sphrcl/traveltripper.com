d57f102441c4d473c190d35933f03b6aacdeae01
jeromemisfit <jerome@misfit-inc.com>
2015-10-15 23:45:04 +0800

    footer: Change to Products & Solutions

--- new-html/footer.php
+++ new-html/footer.php
@@ -22,7 +22,7 @@
 							<li><a href="terms.php">Terms of Service</a></li>
 							<li><a href="privacy-policy.php">Privacy Policy</a></li> -->
 							<li><a href="aboutus.php">Company</a></li>
-							<li><a href="product-and-services.php">Product &amp; Services</a></li>
+							<li><a href="product-and-services.php">Products &amp; Solutions</a></li>
 							<li><a href="careers.php">Careers</a></li>
 							<!-- <li><a href="terms-of-service.php">Terms of Service</a></li>
 							<li><a href="privacy-policy.php">Privacy Policy</a></li> -->
